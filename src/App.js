521b04f195c740e980573cd86398e1408834278c
Firuzakerimova <bd7mbuwz1@code.edu.az>
2024-01-15 00:57:50 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+      
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      
+      </div>
+    </Router>
+  );
+};
+
+export default App;
